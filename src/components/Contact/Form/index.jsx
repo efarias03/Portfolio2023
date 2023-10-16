824775ae65a13075e6fc110bd8f11127bb8b304e
Enzo Farias <enzofarias030603@gmail.com>
2023-10-16 15:46:56 -0300

    finished sucess notification

--- src/components/Contact/Form/index.jsx
+++ src/components/Contact/Form/index.jsx
@@ -5,7 +5,7 @@ export function ContactForm() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
-    const [status, setStatus] = useState("Notification")
+    const [status, setStatus] = useState("")
 
     function sendEmail(e) {
         e.preventDefault()
@@ -26,6 +26,11 @@ export function ContactForm() {
                 setName("")
                 setEmail("")
                 setMessage("")
+                setStatus("sucess")
+
+                setTimeout(() => {
+                    setStatus("")
+                }, 2500);
             })
             .catch((err) => {
                 alert(err)
@@ -34,9 +39,9 @@ export function ContactForm() {
 
     return (
         <form action="" onSubmit={sendEmail}>
-            <div className="notification">
+            <div className={`notification ${status}`}>
                 <div className="notification-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
                 </div>
                 <div className="notification-content">
                     <div className="notification-title">
@@ -51,11 +56,11 @@ export function ContactForm() {
                 <div className="inputs">
                     <div className="input-group">
                         <label htmlFor="name" >Your name here</label>
-                        <input value={name} name="name" id="name" type="text" onChange={(e) => setName(e.target.value)} />
+                        <input value={name} name="name" id="name" type="text" onChange={(e) => setName(e.target.value)} required />
                     </div>
                     <div className="input-group">
                         <label htmlFor="email" >Your email here</label>
-                        <input value={email} name="email" id="email" type="text" onChange={(e) => setEmail(e.target.value)} />
+                        <input value={email} name="email" id="email" type="text" onChange={(e) => setEmail(e.target.value)} required />
                     </div>
                 </div>
                 <div className="message-div">
@@ -63,7 +68,7 @@ export function ContactForm() {
                         <label htmlFor="message">Your cool message:</label>
                     </div>
                     <div className="message-text">
-                        <textarea value={message} name="message" id="message" onChange={(e) => setMessage(e.target.value)} ></textarea>
+                        <textarea value={message} name="message" id="message" onChange={(e) => setMessage(e.target.value)} required ></textarea>
                     </div>
                 </div>
                 <div className="form-button-div">
